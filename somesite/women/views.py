2d573d1cdb80d7177a56ef1bdc2a71883ce3a4b8
Izvekov Alexander <izvekov.web@yandex.ru>
2022-07-19 20:06:27 +0300

    Добавлена форма addpage

--- somesite/women/views.py
+++ somesite/women/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .forms import AddPostForm
 from .models import *
 
 
@@ -20,7 +21,18 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse('Добавление статьи')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'title': 'Добавление статьи'})
 
 
 def contact(request):
