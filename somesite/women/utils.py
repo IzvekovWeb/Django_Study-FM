280ad4788ceba465c4f9380e4d5d4a6c7ccf9cc8
Izvekov Alexander <izvekov.web@yandex.ru>
2022-07-21 15:09:23 +0300

    Добавлена автооризация и регистрация пользователей

--- somesite/women/utils.py
+++ somesite/women/utils.py
@@ -5,8 +5,7 @@ from .models import *
 menu = [
         {'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статьи', 'url_name': 'add_page'},
-        {'title': 'Обратная связь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name': 'login'}
+        {'title': 'Обратная связь', 'url_name': 'contact'}
 ]
 
 
--- somesite/women/views.py
+++ somesite/women/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
-from .forms import AddPostForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm
 from .models import *
 from .utils import *
 
@@ -70,10 +72,6 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 
 def contact(request):
     return HttpResponse('Обратная связь')
-
-
-def login(request):
-    return HttpResponse('Авторизация')
 
 
 class ShowPost(DataMixin, DetailView):
@@ -148,3 +146,37 @@ class WomenCategory(DataMixin, ListView):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
