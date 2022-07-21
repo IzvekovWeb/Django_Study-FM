12cf496d4448e50758ae2385b6c73818c7a11ce5
Izvekov Alexander <izvekov.web@yandex.ru>
2022-07-21 17:02:13 +0300

    Добавлено кэширование

--- somesite/women/utils.py
+++ somesite/women/utils.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.db.models import Count
 
 from .models import *
@@ -10,11 +11,16 @@ menu = [
 
 
 class DataMixin:
-    paginate_by = 30
+    paginate_by = 3
 
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('women'))
+
+        # Кэширование запроса
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
